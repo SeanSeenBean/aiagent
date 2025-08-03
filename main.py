e6db9af3f2830073b3e72aff833a1e4e0ff097b4
SeanSeenBean <routhsean@proton.me>
2025-08-03 18:43:46 -0500

    Initial prompt is now passed in as a command line argument, added a '--verbose' command line flag

--- main.py
+++ main.py
@@ -1,19 +1,35 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
+user_prompt = sys.argv[1]
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+]
 
 def main():
-    response = client.models.generate_content(
-        model="gemini-2.0-flash-001",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    if len(sys.argv) >= 2:
+        response = client.models.generate_content(
+            model="gemini-2.0-flash-001",
+            contents=messages)
+    else:
+        raise Exception("Invalid Input, program requires a single argument")
+        sys.exit(1)
+    if "--verbose" in sys.argv:
+        print(
+            f"User prompt: {user_prompt} \n"
+            f"Prompt tokens: {response.usage_metadata.prompt_token_count} \n"
+            f"Response tokens: {response.usage_metadata.candidates_token_count} \n"
+        )
+
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":
