643a5455f609769838732357a04b21e840b90041
SeanSeenBean <routhsean@proton.me>
2025-09-02 18:19:27 -0500

    Gave agent the ability to loop to make multiple function calls when necessary. Agent now has a hard iterations limit of 20 iterations to prevent an infinite loop.

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from prompts import system_prompt
 from functions.call_function import call_function, available_functions
 
 
+
 def main():
     load_dotenv()
         
@@ -36,10 +37,17 @@ def main():
         
     ]
     
-    generate_content(client, messages, verbose)
-    print(f"messages: {messages}")
-    
-
+    for i in range(20):
+        try:
+            # print(messages)
+            response = generate_content(client, messages, verbose)
+            # print(f"response: {response}")
+            if "Text Response:" in str(response):
+                print(f"Final response {response}")
+                break
+        except Exception as e:
+            return f"Error generating content: {e}"
+            
 
     
 def generate_content(client, messages, verbose):
@@ -51,20 +59,28 @@ def generate_content(client, messages, verbose):
             system_instruction=system_prompt
         ),
     )
-    messages.append(types.Content(role="model", parts=[types.Part(text=response.text)]))
+    
     if verbose:
         print(
             f"Prompt tokens: {response.usage_metadata.prompt_token_count} \n"
             f"Response tokens: {response.usage_metadata.candidates_token_count} \n"
         )
         
+    if response.candidates:
+        for candidate in response.candidates:
+            messages.append(candidate.content)
+            
+        
     if not response.function_calls:
-        return f"Text Response:\n{response.text}"
+        return response.text
+        
+        
                 
     function_responses = []
     for function_call in response.function_calls:
-        # print(f"Calling function: {function_call.name}({function_call.args})")
+        
         function_call_result = call_function(function_call, verbose)
+        
         if (
             not function_call_result.parts
             or not function_call_result.parts[0].function_response
@@ -79,6 +95,8 @@ def generate_content(client, messages, verbose):
             
     if not function_responses:
         raise Exception("no function responses generated, exiting")
+    
+    messages.append(types.Content(role="user", parts = function_responses))
 
 if __name__ == "__main__":
     main()
